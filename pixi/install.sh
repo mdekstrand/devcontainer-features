618d6c01b582ce1dca0435e9fc829eeaba356d1c
Michael Ekstrand <mdekstrand@drexel.edu>
2024-10-14 10:26:58 +0200

    try to get started on features

---
+++ pixi/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -xeuo pipefail
+
+PIXI_ARCH=$(uname -m | sed -e 's/arm64/aarch64/')
+
+GH_REPO="prefix-dev/pixi"
+GH_RELEASE_DL="https://github.com/${GH_REPO}/releases/latest/download/"
+
+FILE="pixi-${PIXI_ARCH}-unknown-linux-musl.tar.gz"
+
+mkdir -p "$HOME/.pixi/bin"
+curl -fsSL "$GH_RELEASE_DL/$FILE" | tar xvzf - -C "$HOME/.pixi/bin"
