92a53d8305aa10702ad9ad56eef4467e6d576e3a
Michael Ekstrand <md@ekstrandom.net>
2024-11-03 12:07:11 -0500

    working devcontainer initialization

--- src/pixi/install.sh
+++ src/pixi/install.sh
@@ -15,3 +15,11 @@ curl -fsSL "$GH_RELEASE_DL/$FILE" | tar xvzf - -C /opt/pixi/bin
 cat >"/opt/pixi/config.toml" <<EOF
 detached-environments = "/opt/pixi/envs"
 EOF
+
+cat >"/opt/pixi/devcontainer-init-env.sh" <<EOF
+#!/bin/sh
+pixi_env="$ENVIRONMENT"
+if [ ! -z "\$pixi_env" ]; then
+    pixi install -e "\$pixi_env" || exit 3
+fi
+EOF
